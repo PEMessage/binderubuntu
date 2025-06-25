cb2caade3e4d5718705fddb8360489615c2e269e
PEMessage <1165739182@qq.com>
2025-06-26 00:08:14 +0800

    add project.sh setup cmds

--- project.sh
+++ project.sh
@@ -2,6 +2,14 @@
 
 cmd="${1}" 
 case ${cmd} in 
+    # also see q-task: mount-binderfs.sh
+    setup)
+        sh -c 'sudo mkdir -p /dev/binderfs'
+        sh -c 'sudo mount -t binder binder /dev/binderfs'
+        sh -c 'sudo chmod 0666 /dev/binderfs/*'
+        sh -c 'sudo chmod 0666 /dev/ashmem'
+        sh -c 'sudo ln -s /dev/binderfs/* /dev/'
+        ;;
     insmod)
         sh -c 'sudo insmod driver/binder/binder_linux.ko'
         sh -c 'sudo insmod driver/ashmem/ashmem_linux.ko'
@@ -11,7 +19,7 @@ case ${cmd} in
         sh -c 'sudo rmmod ashmem_linux'
         ;; 
    *)  
-      echo "`basename ${0}`:usage: [insmod] | [rmmod]" 
+      echo "`basename ${0}`:usage: [setup] | [insmod] | [rmmod]" 
       exit 1
       ;; 
 esac
